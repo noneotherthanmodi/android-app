e013ce68fbd31a9c22ca2deb0ed6a37794d793bd
Udit <uditmodi03@gmail.com>
2023-11-11 02:44:27 +0530

    simple birthday wish app

--- app/src/main/java/com/example/birthday/MainActivity.kt
+++ app/src/main/java/com/example/birthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.birthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,11 +15,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthday.ui.theme.BirthdayTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +35,12 @@ class MainActivity : ComponentActivity() {
             BirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    EnterText(message = "Welcome to the app Ram", from = "from Modi")
+                    GreetingImage(message = stringResource(R.string.welcome_to_the_app_text), from = stringResource(
+                        R.string.signature_text
+                    ))
                 }
             }
         }
@@ -52,19 +62,38 @@ fun EnterText(message: String,from: String, modifier: Modifier = Modifier){
             fontSize = 30.sp,
             modifier = Modifier
                 .padding(8.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
 
         )
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String,modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop, //fits the image to the screen
+            alpha = 0.7F //controls opacity
+        )
+        EnterText(message = message, from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true,
-    showSystemUi = true, //changes to phone view
+//    showSystemUi = true, //changes to phone view
     name = "My preview")
 @Composable
 fun BirthdayCardPreview() {
     BirthdayTheme {
-        EnterText(message = "Welcome to the app Ram!", from = "From Modi")
+        GreetingImage(message = stringResource(R.string.welcome_to_the_app_text), from = stringResource(
+            R.string.signature_text
+        ))
     }
 }
